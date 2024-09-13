358a7fe6c6b3d0926a672a276e6e11c17a4b8e8f
JoCh Soro <joch.soro@gmail.com>
2024-09-14 00:23:36 +0200

    TP5

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,11 +1,8 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.entity.Movie;
-import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
-import com.mycompany.dvdstore.service.MovieService;
-
-import java.util.Scanner;
+import com.mycompany.dvdstore.repository.FileMovieRepository;
+import com.mycompany.dvdstore.service.DefaultMovieService;
 
 /**
  * Hello world!
@@ -16,10 +13,10 @@ public class App
     public static void main( String[] args )
     {
         MovieController movieController=new MovieController();
-        GoLiveMovieRepository movieRepository=new GoLiveMovieRepository();
-        MovieService movieService=new MovieService();
-        movieController.setMovieService(movieService);
-        movieService.setMovieRepository(movieRepository);
+        FileMovieRepository movieRepository=new FileMovieRepository();
+        DefaultMovieService defaultMovieService =new DefaultMovieService();
+        movieController.setMovieService(defaultMovieService);
+        defaultMovieService.setMovieRepository(movieRepository);
         movieController.addUsingConsole();
     }
 }
--- src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
+++ src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
@@ -1,11 +1,9 @@
 package com.mycompany.dvdstore.service;
 
 import com.mycompany.dvdstore.entity.Movie;
-import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
-import com.mycompany.dvdstore.repository.MovieRepository;
 import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 
-public class MovieService implements MovieServiceInterface{
+public class DefaultMovieService implements MovieServiceInterface{
 
     private MovieRepositoryInterface movieRepository;
 
