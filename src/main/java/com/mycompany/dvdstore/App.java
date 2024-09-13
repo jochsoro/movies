91e1ee021a135606df2c4d4bfb85bf1b8d460c5f
JoCh Soro <joch.soro@gmail.com>
2024-09-13 23:38:53 +0200

    TP4

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,6 +1,8 @@
 package com.mycompany.dvdstore;
 
+import com.mycompany.dvdstore.controller.MovieController;
 import com.mycompany.dvdstore.entity.Movie;
+import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
 import com.mycompany.dvdstore.service.MovieService;
 
 import java.util.Scanner;
@@ -13,15 +15,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Quel est le titre du film ?" );
-        Scanner scanner=new Scanner(System.in);
-        String title=scanner.nextLine();
-        System.out.println( "Quel est le genre du film ?" );
-        String genre=scanner.nextLine();
-        Movie movie=new Movie();
-        movie.setTitle(title);
-        movie.setGenre(genre);
+        MovieController movieController=new MovieController();
+        GoLiveMovieRepository movieRepository=new GoLiveMovieRepository();
         MovieService movieService=new MovieService();
-        movieService.registerMovie(movie);
+        movieController.setMovieService(movieService);
+        movieService.setMovieRepository(movieRepository);
+        movieController.addUsingConsole();
     }
 }
--- src/main/java/com/mycompany/dvdstore/service/MovieService.java
+++ src/main/java/com/mycompany/dvdstore/service/MovieService.java
@@ -9,6 +9,14 @@ public class MovieService implements MovieServiceInterface{
 
     private MovieRepositoryInterface movieRepository;
 
+    public MovieRepositoryInterface getMovieRepository() {
+        return movieRepository;
+    }
+
+    public void setMovieRepository(MovieRepositoryInterface movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
     public void registerMovie(Movie movie){
         movieRepository.add(movie);
     }
